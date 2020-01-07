7a8a4d1c554de879f482137b6f028bdef4a54513
Maria Toneva <maria.v.toneva@abv.bg>
2020-01-07 11:27:56 +0200

    Point in Rectangle

---
+++ C# OOP/PointInRectangle/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿namespace PointInRectangle
+{
+    public class Rectangle
+    {
+        public Rectangle(int topLeftX, int topLeftY, int bottomRightX, int bottomRightY)
+        {
+            this.TopLeft = new Point(topLeftX, topLeftY);
+            this.BottomRight = new Point(bottomRightX, bottomRightY);
+        }
+
+        public Point TopLeft { get; set; }
+        public Point BottomRight { get; set; }
+
+        public bool Contains(Point point)
+        {
+            if (point.X >= this.TopLeft.X
+                && point.X <= this.BottomRight.X
+                && point.Y >= this.TopLeft.Y
+                && point.Y <= this.BottomRight.Y)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
