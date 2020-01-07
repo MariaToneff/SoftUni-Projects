c724e97fc4b13adbffe7ec582e35c92798f49a77
Maria Toneva <maria.v.toneva@abv.bg>
2020-01-07 17:38:31 +0200

    Hotel Reservation

---
+++ C# OOP/WorkingWithAbstraction/Hotel Reservation/PriceCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HotelReservation
+{
+    public class PriceCalculator
+    {
+        public PriceCalculator(string[] input)
+        {
+            this.PricePerDay = double.Parse(input[0]);
+            this.NumberOfDays = int.Parse(input[1]);
+            this.Season = Enum.Parse<Season>(input[2]);
+            this.DiscountType = DiscountType.None;
+
+            if (input.Length > 3)
+            {
+                this.DiscountType = Enum.Parse<DiscountType>(input[3]);
+            }
+        }
+        public double PricePerDay { get; set; }
+        public int NumberOfDays { get; set; }
+        public Season Season { get; set; }
+        public DiscountType DiscountType { get; set; }
+        public double GetTotalPrice()
+        {
+            var price = this.PricePerDay * this.NumberOfDays * (int)this.Season;
+            var totalPrice = price - (price * (int)this.DiscountType / 100);
+            return totalPrice;
+        }
+    }
+}
---
+++ C# OOP/WorkingWithAbstraction/Hotel Reservation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HotelReservation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+            var priceCalculator = new PriceCalculator(input);
+
+            var result = priceCalculator.GetTotalPrice();
+
+            Console.WriteLine($"{result:F2}");
+        }
+    }
+}
